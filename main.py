a316df6da9b0017e8069c92acb9a0c67cacc4877
[Mohamed <MansourMohamedAli1996@gmail.com>
2023-12-02 18:02:51 -0500

    Added Scrolling to design feature list created main and configuration page.

--- main.py
+++ main.py
@@ -3,42 +3,82 @@ from tkinter import ttk
 
 
 class App(tk.Tk):
-    def __init__(self, title, dimensions):
-        # main setup
-        super().__init__()
-        self.title(title)
-        self.geometry(f"{dimensions[0]}x{dimensions[1]}")
-        self.minsize(600, 600)
+	def __init__(self, title, dimensions):
+		# main setup
+		super().__init__()
+		self.title(title)
+		self.geometry(f"{dimensions[0]}x{dimensions[1]}")
+		self.minsize(600, 600)
+		self.maxsize(900, 800)
 
-        # Widgets
-        self.menu = Menu(self)
+		# Widgets
+		self.menu = Menu(self)
 
-        # Run
-        self.mainloop()
+
+		# Run
+		self.mainloop()
 
 
 class Menu(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        ttk.Label(self, background='red').pack(expand=True, fill='both')
-        self.place(x=0, y=0, relwidth=0.3, relheight=1)
+	def __init__(self, parent):
+		super().__init__(parent)
+		self.place(x = 0, y = 0, relwidth = 1, relheight = 1)
 
-        self.create_widgets()
+		global open_config
+		def open_config():
+			Configuration()
+		self.create_widgets()
 
-    def create_widgets(self):
-        # create the widgets
-        menu_button1 = ttk.Button(self, text='Button 1')
-        menu_button2 = ttk.Button(self, text='Button 2')
-        menu_button3 = ttk.Button(self, text='Button 3')
 
-        menu_slider1 = ttk.Scale(self, orient='vertical')
-        menu_slider2 = ttk.Scale(self, orient='vertical')
+	def create_widgets(self):
+		menu_button1 = ttk.Button(self, text = 'Button 1')
+		menu_button2 = ttk.Button(self, text = 'Button 2')
+		menu_button3 = ttk.Button(self, text = 'Button 3')
+		menu_button4 = ttk.Button(self, text = 'Button 4')
+		menu_button5 = ttk.Button(self, text = 'Button 5')
+		menu_button6 = ttk.Button(self, text = 'Button 6')
+		config_button = ttk.Button(self, text = 'Configuration', command = open_config)
 
-        toggle_frame = ttk.Frame(self)
-        menu_toggle1 = ttk.Checkbutton(toggle_frame, text='check 1')
-        menu_toggle2 = ttk.Checkbutton(toggle_frame, text='check 2')
+		# create the grid
+		self.columnconfigure((0,1), weight = 1, uniform = 'a')
+		self.rowconfigure((0,1,2,3), weight = 1, uniform = 'a')
 
-        entry = ttk.Entry(self)
+		# place the widgets 
+		menu_button1.grid(row = 0, column = 0, sticky = 'nsew', columnspan = 1, padx=(10,5),pady=(10,10))
+		menu_button2.grid(row = 0, column = 1, sticky = 'nsew', columnspan = 1, padx=(5,10),pady=(10,10))
+		menu_button3.grid(row = 1, column = 0, sticky = 'nsew', columnspan = 1, padx=(10,5),pady=(10,10))
+		menu_button4.grid(row = 1, column = 1, sticky = 'nsew', columnspan = 1, padx=(5,10),pady=(10,10))
+		menu_button5.grid(row = 2, column = 0, sticky = 'nsew', columnspan = 1, padx=(10,5),pady=(10,10))
+		menu_button6.grid(row = 2, column = 1, sticky = 'nsew', columnspan = 1, padx=(5,10),pady=(10,10))
+
+		config_button.grid(row = 3, column = 0, sticky = 'nsew', columnspan = 2, padx=(5,10),pady=(10,10))
+
+class Configuration(tk.Toplevel):
+	def __init__(self):
+		super().__init__()
+		self.title('Configuration')
+		self.geometry("800x600")
+		
+		self.create_widgets()
+		
+	def create_widgets(self):
+		button_tabs = ttk.Notebook(self)
+		
+		#tab 1
+		tab1 = ttk.Frame(button_tabs)
+		tab2 = ttk.Frame(button_tabs)
+		
+		button_tabs.add(tab1, text = 'Tab 1')
+		button_tabs.add(tab2, text = 'Tab 2')
+		
+		button_tabs.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+
+
+
+
+
+
+
 
 
 App('Glass Panel Control', (600, 600))
