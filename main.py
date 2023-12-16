88d55f00bf3fe1a6ac0b4be230b27b1697c3d76d
[Mohamed <MansourMohamedAli1996@gmail.com>
2023-12-16 16:05:25 -0500

    Added frames to configuration pages. Made tab widget expand in tabFrame.

--- main.py
+++ main.py
@@ -3,82 +3,105 @@ from tkinter import ttk
 
 
 class App(tk.Tk):
-	def __init__(self, title, dimensions):
-		# main setup
-		super().__init__()
-		self.title(title)
-		self.geometry(f"{dimensions[0]}x{dimensions[1]}")
-		self.minsize(600, 600)
-		self.maxsize(900, 800)
+    def __init__(self, title, dimensions):
+        # main setup
+        super().__init__()
+        self.title(title)
+        self.geometry(f"{dimensions[0]}x{dimensions[1]}")
+        self.minsize(600, 600)
+        self.maxsize(900, 800)
 
-		# Widgets
-		self.menu = Menu(self)
+        # Widgets
+        self.menu = Menu(self)
 
-
-		# Run
-		self.mainloop()
+        # Run
+        self.mainloop()
 
 
 class Menu(ttk.Frame):
-	def __init__(self, parent):
-		super().__init__(parent)
-		self.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(x=0, y=0, relwidth=1, relheight=1)
 
-		global open_config
-		def open_config():
-			Configuration()
-		self.create_widgets()
+        global open_config
 
+        def open_config():
+            Configuration()
 
-	def create_widgets(self):
-		menu_button1 = ttk.Button(self, text = 'Button 1')
-		menu_button2 = ttk.Button(self, text = 'Button 2')
-		menu_button3 = ttk.Button(self, text = 'Button 3')
-		menu_button4 = ttk.Button(self, text = 'Button 4')
-		menu_button5 = ttk.Button(self, text = 'Button 5')
-		menu_button6 = ttk.Button(self, text = 'Button 6')
-		config_button = ttk.Button(self, text = 'Configuration', command = open_config)
+        self.create_widgets()
 
-		# create the grid
-		self.columnconfigure((0,1), weight = 1, uniform = 'a')
-		self.rowconfigure((0,1,2,3), weight = 1, uniform = 'a')
+    def create_widgets(self):
+        menu_button1 = ttk.Button(self, text='Button 1')
+        menu_button2 = ttk.Button(self, text='Button 2')
+        menu_button3 = ttk.Button(self, text='Button 3')
+        menu_button4 = ttk.Button(self, text='Button 4')
+        menu_button5 = ttk.Button(self, text='Button 5')
+        menu_button6 = ttk.Button(self, text='Button 6')
+        config_button = ttk.Button(self, text='Configuration', command=open_config)
 
-		# place the widgets 
-		menu_button1.grid(row = 0, column = 0, sticky = 'nsew', columnspan = 1, padx=(10,5),pady=(10,10))
-		menu_button2.grid(row = 0, column = 1, sticky = 'nsew', columnspan = 1, padx=(5,10),pady=(10,10))
-		menu_button3.grid(row = 1, column = 0, sticky = 'nsew', columnspan = 1, padx=(10,5),pady=(10,10))
-		menu_button4.grid(row = 1, column = 1, sticky = 'nsew', columnspan = 1, padx=(5,10),pady=(10,10))
-		menu_button5.grid(row = 2, column = 0, sticky = 'nsew', columnspan = 1, padx=(10,5),pady=(10,10))
-		menu_button6.grid(row = 2, column = 1, sticky = 'nsew', columnspan = 1, padx=(5,10),pady=(10,10))
+        # create the grid
+        self.columnconfigure((0, 1), weight=1, uniform='a')
+        self.rowconfigure((0, 1, 2, 3), weight=1, uniform='a')
 
-		config_button.grid(row = 3, column = 0, sticky = 'nsew', columnspan = 2, padx=(5,10),pady=(10,10))
+        # place the widgets
+        menu_button1.grid(row=0, column=0, sticky='nsew', columnspan=1, padx=(10, 5), pady=(10, 10))
+        menu_button2.grid(row=0, column=1, sticky='nsew', columnspan=1, padx=(5, 10), pady=(10, 10))
+        menu_button3.grid(row=1, column=0, sticky='nsew', columnspan=1, padx=(10, 5), pady=(10, 10))
+        menu_button4.grid(row=1, column=1, sticky='nsew', columnspan=1, padx=(5, 10), pady=(10, 10))
+        menu_button5.grid(row=2, column=0, sticky='nsew', columnspan=1, padx=(10, 5), pady=(10, 10))
+        menu_button6.grid(row=2, column=1, sticky='nsew', columnspan=1, padx=(5, 10), pady=(10, 10))
+        config_button.grid(row=3, column=0, sticky='nsew', columnspan=2, padx=(5, 10), pady=(10, 10))
+
 
 class Configuration(tk.Toplevel):
-	def __init__(self):
-		super().__init__()
-		self.title('Configuration')
-		self.geometry("800x600")
-		
-		self.create_widgets()
-		
-	def create_widgets(self):
-		button_tabs = ttk.Notebook(self)
-		
-		#tab 1
-		tab1 = ttk.Frame(button_tabs)
-		tab2 = ttk.Frame(button_tabs)
-		
-		button_tabs.add(tab1, text = 'Tab 1')
-		button_tabs.add(tab2, text = 'Tab 2')
-		
-		button_tabs.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+    def __init__(self):
+        super().__init__()
+        self.title('Configuration')
+        self.geometry("800x600")
 
+        self.create_widgets()
 
+    def create_widgets(self):
+        """All Frames that make up Configuration Window"""
+        tabFrame = ttk.Frame(self, relief=tk.GROOVE)
+        sideBarFrame = ttk.Frame(self, relief=tk.GROOVE)
+        topBarFrame = ttk.Frame(self, relief=tk.GROOVE)
+        botBarFrame = ttk.Frame(self, relief=tk.GROOVE)
 
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=5)
+        self.rowconfigure(2, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=5)
 
+        """Tab Frame configuration"""
+        tabs = ttk.Notebook(tabFrame, width=tabFrame.winfo_width(), height=tabFrame.winfo_height())
+        tabFrame.rowconfigure(0, weight=1)
+        tabFrame.columnconfigure(0, weight=1)
 
+        tab1 = tk.Frame(tabs, bg='red')
+        tab2 = tk.Frame(tabs, bg='green')
 
+        tabs.add(tab1, text='First Tab')
+        tabs.add(tab2, text='Second Tab')
 
+        tabs.grid(sticky='nsew')
+
+        """Side Bar Configuration"""
+        sideLabel = ttk.Label(sideBarFrame, text="Side Bar")
+        sideLabel.pack(expand=True)
+
+        """top Bar Configuration"""
+        topLabel = ttk.Label(topBarFrame, text="Top Bar")
+        topLabel.pack(expand=True)
+        """bot Bar Configuration"""
+        botLabel = ttk.Label(botBarFrame, text="Bottom Bar")
+        botLabel.pack(expand=True)
+
+        tabFrame.grid(row=1, column=1, sticky='nsew', padx=(10, 5), pady=(10, 10))
+        sideBarFrame.grid(row=0, column=0, sticky='nsew', rowspan=3, padx=(10, 5), pady=(10, 10))
+        topBarFrame.grid(row=0, column=1, sticky='nsew', padx=(10, 5), pady=(10, 10))
+        botBarFrame.grid(row=2, column=1, sticky='nsew', padx=(10, 5), pady=(10, 10))
 
 
 App('Glass Panel Control', (600, 600))
