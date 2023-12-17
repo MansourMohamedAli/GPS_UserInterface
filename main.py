f722e9a53993a2e2aed0b59c8ff5b2f158b5a782
[Mohamed <MansourMohamedAli1996@gmail.com>
2023-12-16 22:54:35 -0500

    Added Scroll widget and lists to fill scroll.

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ class Configuration(tk.Toplevel):
         super().__init__()
         self.title('Configuration')
         self.geometry("800x600")
+        self.minsize(400, 300)
 
         self.create_widgets()
 
@@ -79,9 +80,17 @@ class Configuration(tk.Toplevel):
         tabFrame.rowconfigure(0, weight=1)
         tabFrame.columnconfigure(0, weight=1)
 
-        tab1 = tk.Frame(tabs, bg='red')
-        tab2 = tk.Frame(tabs, bg='green')
+        """Creating Tab 1"""
+        tab1 = tk.Frame(tabs)
+        text_list = [('label', 'button'), ('thing', 'click'), ('third', 'something'), ('label1', 'button'),
+                     ('label2', 'button'), ('label3', 'button'), ('label4', 'button')]
+        ScrollFrame(tab1, text_list, 10)
 
+        """Creating Tab 2"""
+        tab2 = tk.Frame(tabs)
+        # ScrollFrame(tab2, text_list, 100)
+
+        """Adding tabs to Tab Notebook Frame"""
         tabs.add(tab1, text='First Tab')
         tabs.add(tab2, text='Second Tab')
 
@@ -102,6 +111,78 @@ class Configuration(tk.Toplevel):
         sideBarFrame.grid(row=0, column=0, sticky='nsew', rowspan=3, padx=(10, 5), pady=(10, 10))
         topBarFrame.grid(row=0, column=1, sticky='nsew', padx=(10, 5), pady=(10, 10))
         botBarFrame.grid(row=2, column=1, sticky='nsew', padx=(10, 5), pady=(10, 10))
+
+
+class ScrollFrame(ttk.Frame):
+    def __init__(self, parent, text_data, item_height):
+        super().__init__(master=parent)
+        self.pack(expand=True, fill='both')
+
+        # widget data
+        self.text_data = text_data
+        self.item_number = len(text_data)
+        # self.list_height = (self.item_number * item_height) / 5  # Five items per row
+        self.list_height = (20 * item_height) / 5  # Test of 20 items
+
+        # canvas
+        self.canvas = tk.Canvas(self, background='red', scrollregion=(0, 0, self.winfo_width(), self.list_height))
+        self.canvas.pack(expand=True, fill='both')
+
+        # display frame
+        self.frame = ttk.Frame(self)
+
+        row = 0
+        column = 0
+        for item in range(self.item_number):
+            if column > 4:
+                row += 1  # Increment the row
+                column = 0  # Set column back to 0
+            # self.create_item(index, item).pack(expand=True, fill='both', pady=4, padx=10, side='right')
+            self.create_item(column, item).grid(row=row, column=column, sticky='nsew')
+            column += 1
+
+        # scrollbar
+        self.scrollbar = ttk.Scrollbar(self, orient='vertical', command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
+
+        # events
+        self.canvas.bind_all('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta / 60), "units"))
+        self.bind('<Configure>', self.update_size)
+
+    def update_size(self, event):
+        if self.list_height >= self.winfo_height():
+            height = self.list_height
+            self.canvas.bind_all('<MouseWheel>',
+                                 lambda event: self.canvas.yview_scroll(-int(event.delta / 60), "units"))
+            self.scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
+        else:
+            height = self.winfo_height()
+            self.canvas.unbind_all('<MouseWheel>')
+            self.scrollbar.place_forget()
+
+        self.canvas.create_window(
+            (0, 0),
+            window=self.frame,
+            anchor='nw',
+            width=self.winfo_width(),
+            height=height)
+
+    def create_item(self, index, item):
+        frame = ttk.Frame(self.frame)
+
+        # grid layout
+        # frame.rowconfigure(0, weight=1)
+        # frame.columnconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
+
+        # widgets
+        # tk.Listbox(frame, bg='red').grid(row=0, column=index)
+        tk.Listbox(frame, bg='red').pack(expand=True, fill='both')
+        # ttk.Label(frame, text=f'#{index}').grid(row=0, column=0)
+        # ttk.Label(frame, text=f'{item[0]}').grid(row=0, column=1)
+        # ttk.Button(frame, text=f'{item[1]}').grid(row=0, column=2, columnspan=3, sticky='nsew')
+
+        return frame
 
 
 App('Glass Panel Control', (600, 600))
