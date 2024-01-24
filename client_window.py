3d63b90f498504166e7d2656141f012354df4860
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2024-01-23 22:01:09 -0500

    Formatting client window.

--- client_window.py
+++ client_window.py
@@ -7,31 +7,37 @@ class ClientWindow(tk.Toplevel):
         super().__init__()
         self.m_insert_client = m_insert_client
         self.m_insert_another_client = m_insert_another_client
+
         self.title('Client Configuration')
         self.geometry("500x200")
         self.resizable(False, False)
 
-        # Frame for left text
-        self.labels_frame = ttk.Frame(self)
-        # Command Name Label
-        self.client_name_label = ttk.Label(self.labels_frame, text="Client Name:")
-        # Command Name Label
-        self.ip_address = ttk.Label(self.labels_frame, text="IP Address:")
-        # Placing Labels
-        self.client_name_label.place(relx=0.1, rely=0.1)
-        self.ip_address.place(relx=0.1, rely=0.30)
-        # Frame for text entries
-        self.text_frame = ttk.Frame(self)
-        # Text box for commands
-        self.client_name_entry = tk.Entry(self.text_frame, width=53)
-        # Placing Text Boxes
-        self.client_name_entry.place(relx=0, rely=0.1)
+        # Client
+        self.client_frame = ttk.Frame(self)
+        self.client_name_label = ttk.Label(self.client_frame, text="Client Name:")
+        self.client_name_entry = tk.Entry(self.client_frame, width=53)
 
-        # Button Frame
+        self.client_name_label.place(relx=0.1, rely=0.5)
+        self.client_name_entry.place(relx=0.30, rely=0.5)
+
+        # IP
+        self.ip_frame = ttk.Frame(self)
+        self.ip_label = ttk.Label(self.ip_frame, text="IP Address:")
+        self.ip_entry = tk.Entry(self.ip_frame, width=53)
+
+        self.ip_label.place(relx=0.1, rely=0.5)
+        self.ip_entry.place(relx=0.30, rely=0.5)
+
+        # MAC
+        self.mac_frame = ttk.Frame(self)
+        self.mac_label = ttk.Label(self.mac_frame, text="MAC Address:")
+        self.mac_entry = tk.Entry(self.mac_frame, width=53)
+
+        self.mac_label.place(relx=0.1, rely=0.5)
+        self.mac_entry.place(relx=0.30, rely=0.5)
+
+        # Buttons
         self.buttons_frame = ttk.Frame(self)
-        # Done button
-
-        #
         self.done_button = ttk.Button(self.buttons_frame,
                                       text="Done",
                                       command=lambda: self.m_insert_client(self, self.client_name_entry.get()))
@@ -40,18 +46,19 @@ class ClientWindow(tk.Toplevel):
                                              text="Add Another",
                                              command=lambda: self.m_insert_another_client(
                                                  self.client_name_entry.get()))
-        # Placing Buttons
-        self.done_button.place(relx=0.125, rely=0)
-        self.add_another_button.place(relx=0.45, rely=0)
+        self.done_button.place(relx=0.32, rely=0.25)
+        self.add_another_button.place(relx=0.58, rely=0.25)
 
         # Configuring Grid
-        self.rowconfigure(0, weight=5)
+        self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=1)
         self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
 
-        # Placing frames in grid
-        self.labels_frame.grid(row=0, column=0, rowspan=2, sticky='nsew')
-        self.text_frame.grid(row=0, column=1, rowspan=2, sticky='nsew')
-        self.buttons_frame.grid(row=1, column=1, sticky='nsew')
+        self.client_frame.grid(row=0, column=0, sticky='nsew')
+        self.ip_frame.grid(row=1, column=0, sticky='nsew')
+        self.mac_frame.grid(row=2, column=0, sticky='nsew')
+        self.buttons_frame.grid(row=3, column=0, sticky='nsew')
+
 
