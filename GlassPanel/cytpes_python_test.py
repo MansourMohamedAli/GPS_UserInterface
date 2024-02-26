e5f2a5c0a4d55963e5f9fee463a59e20f318548c
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2024-02-25 19:11:34 -0500

    Can now send commands from python to SendCmdServer.exe. I have to specify number of arguments on python side but will work to remove that.

--- GlassPanel/cytpes_python_test.py
+++ GlassPanel/cytpes_python_test.py
@@ -1,19 +1,9 @@
-#from ctypes import CDLL, c_int, c_char_p
-from ctypes import *
+from ctypes import CDLL, c_int, c_char_p
 clibrary = CDLL("C:/Projects/Python/GPS_UserInterface/GlassPanel/SendCmd/SendCmdClient/debug/SendCmdClient.dll")
 
-#func = clibrary.python_display_int_arg
-#func.argtypes = [c_int, c_int]
-#func.restype = c_int
-#print(func(2, 7))
+send_cmd_func = clibrary.send_cmd
+send_cmd_func.argtypes = [c_int, c_char_p]
+send_cmd_func.restype = c_int
 
-#func2 = clibrary.python_display_void_return
-#func2.argtypes = [c_int, c_int]
-#func2.restype = None
-#func2(2, 5)
-
-func2 = clibrary.main
-func2.argtypes = [c_int, c_char_p]
-func2.restype = c_int
-print(func2(2, "Test"))
-
+command = c_char_p(b"10.0.0.132 notepad")
+send_cmd_func(2, command)
